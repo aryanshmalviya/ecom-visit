cbae24592c8d1c19ecf457eb9a0ebe4649a1307f
aryanshmalviya <aryanshmalviya2626@gmail.com>
2024-12-20 14:14:48 +0530

    Getting started with sanity and clerk

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "../globals.css"
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider dynamic>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
