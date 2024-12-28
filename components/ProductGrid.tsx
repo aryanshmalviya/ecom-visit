2b295b7e14240efb157162d19b191b12548849a1
aryanshmalviya <aryanshmalviya2626@gmail.com>
2024-12-28 11:41:51 +0530

    lg screen issue fix

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -6,8 +6,9 @@ import ProductThumb from "./ProductThumb";
 
 
 function ProductGrid({ products }: { products: Product[] }) {
+    const length = products.length
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+        <div className= {`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4 ${length <= 3 ? "lg:grid-cols-3" : "lg:grid-cols-4"}`}>
             {products?.map((product) => {
                 return (
                     <AnimatePresence key={product._id}>
