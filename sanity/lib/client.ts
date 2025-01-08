c6db48d65d451d856f2ebe26e5aaebb1a9fd3d76
aryanshmalviya <aryanshmalviya2626@gmail.com>
2025-01-08 17:09:49 +0530

    sanity viewer development and configuring

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -5,11 +5,11 @@ import { apiVersion, dataset, projectId } from '../env'
 export const client = createClient({
   projectId,
   dataset,
-  apiVersion, 
+  apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  stega:{ 
-    studioUrl: process.env.VERCEL_URL 
-    ? `httpsL//${process.env.VERCEL_URL}/studio`
-    : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
+  stega: {
+    studioUrl:
+      process.env.NODE_ENV === "production" ? `https://${process.env.VERCEL_URL}/studio`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`
   },
 });
